f5d00dfe66190ae9d0371df9129759ead3f03839
DennisD2 <info@spurtikus.de>
2026-08-17 12:27:56 +0200

    build server findings

--- x11/x11_test.go
+++ x11/x11_test.go
@@ -11,8 +11,6 @@ func TestXmStringCreateLtoR(t *testing.T) {
 
 	// Ensure conversion to XtArgVal yields a non-zero value
 	v := XtArgValFromXmString(xs)
-
-	// KORREKTUR: Da v ein uintptr (eine Zahl) ist, prüfen wir auf 0 statt auf nil
 	if v == 0 {
 		t.Fatalf("XtArgValFromXmString returned zero ArgVal")
 	}
