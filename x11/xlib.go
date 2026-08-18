7337b933837fa0e04b311a58d56cb784930ea07a
DennisD2 <info@spurtikus.de>
2026-08-18 16:40:20 +0200

    Made xlib.go only include "xlib.h"

--- x11/xlib.go
+++ x11/xlib.go
@@ -1,12 +1,13 @@
 package x11
 
+// Below is simngle place where we have CFLAGS and LDFLAGS located.
 /* #cgo CFLAGS: -std=c99 -Wno-incompatible-pointer-types
 #cgo LDFLAGS: -lXm -lXt -lX11
 #include <stdint.h>
 #include <stdlib.h>
 
 // Xt includes
-#include <X11/Intrinsic.h>
+#include <X11/Xlib.h>
 */
 import "C"
 
