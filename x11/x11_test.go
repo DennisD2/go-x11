d1a583b55947019f26531bf2dddaa1929ed48508
DennisD2 <info@spurtikus.de>
2026-08-18 17:56:04 +0200

    Fixed test

--- x11/x11_test.go
+++ x11/x11_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestXmStringCreateLtoR(t *testing.T) {
 	text := "hello world"
-	xs := XmStringCreateLtoR(text)
+	xs := XmStringCreateLtoR(text, XmSTRING_DEFAULT_CHARSET)
 	if xs.XmString == nil {
 		t.Fatalf("XmStringCreateLtoR returned nil for %q", text)
 	}
